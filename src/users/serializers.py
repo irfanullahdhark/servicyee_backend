5c7a3cb05bdb4f02a7f032182a73227dac3dfac8
Esmat-Farjad <esmatullah.hashimi2016@gmail.com>
2025-07-22 19:11:42 +0430

    custom register added

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -1,15 +1,33 @@
-from dj_rest_auth.registration.serializers import RegisterSerializer
+# users/serializers.py
+from django.contrib.auth.models import User
+from rest_framework import serializers
 
 
-class CustomRegisterSerializer(RegisterSerializer):
-    username = None  # remove username if not used
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
+    name = serializers.CharField(write_only=True)
 
-    def get_cleaned_data(self):
-        data = super().get_cleaned_data()
-        data.pop("username", None)  # make sure to remove username from cleaned_data
+    class Meta:
+        model = User
+        fields = ["email", "name", "password", "confirm_password"]
+
+    def validate(self, data):
+        if data["password"] != data["confirm_password"]:
+            raise serializers.ValidationError("Passwords do not match.")
         return data
 
-    @property
-    def _has_phone_field(self):
-        # Tell allauth there is no phone field
-        return False
+    def create(self, validated_data):
+        name = validated_data.pop("name")
+        validated_data.pop("confirm_password")
+
+        first_name, *last_name = name.split(" ", 1)
+        user = User(
+            email=validated_data["email"],
+            username=validated_data["email"],  # username is required
+            first_name=first_name,
+            last_name=last_name[0] if last_name else "",
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
--- src/users/urls.py
+++ src/users/urls.py
@@ -1,20 +1,11 @@
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from .views import UserViewSet
-
-# router = DefaultRouter()
-# router.register(r'user', UserViewSet)
-
-# urlpatterns = [
-#     path('', include(router.urls)),
-# ]
-# user/urls.py
 from dj_rest_auth.views import PasswordResetConfirmView, PasswordResetView
 from django.urls import include, path
 
+from users.views import RegisterView
+
 urlpatterns = [
     path("", include("dj_rest_auth.urls")),
-    path("registration/", include("dj_rest_auth.registration.urls")),
+    path("register/", RegisterView.as_view(), name="register"),
     path("password/reset/", PasswordResetView.as_view(), name="password_reset"),
     path(
         "password/reset/confirm/<str:uidb64>/<str:token>/",
