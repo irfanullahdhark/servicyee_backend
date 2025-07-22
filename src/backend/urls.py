193a620df84af899621c4737445f866c90bdae6e
Esmat-Farjad <esmatullah.hashimi2016@gmail.com>
2025-07-22 21:19:14 +0430

    ruff check and black check done

--- src/backend/urls.py
+++ src/backend/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 
 # project/urls.py
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('user.urls')),
-]   
+    path("admin/", admin.site.urls),
+    path("api/v1/", include("user.urls")),
+]
--- src/user/views.py
+++ src/user/views.py
@@ -1,25 +1,29 @@
-from rest_framework import viewsets
-from .models import User
-from .serializers import UserSerializer
-from rest_framework.response import Response
-from rest_framework import status
-import environ
 import os
 
+import environ
+from rest_framework import status, viewsets
+from rest_framework.response import Response
+
+from .models import User
+from .serializers import UserSerializer
+
 env = environ.Env()
-environ.Env.read_env(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '.env'))
+environ.Env.read_env(
+    os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), ".env")
+)
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     keycloak_roles = {
-        'GET': ['admin'],
-        'POST': ['admin'],
-        'PUT': ['admin'],
-        'PATCH': ['admin'],
-        'DELETE': ['admin']
+        "GET": ["admin"],
+        "POST": ["admin"],
+        "PUT": ["admin"],
+        "PATCH": ["admin"],
+        "DELETE": ["admin"],
     }
 
     def list(self, request):
-        print(env('KEYCLOAK_SERVER_URL'))
-        return Response({'message': 'Hello, World!'}, status=status.HTTP_200_OK)
+        print(env("KEYCLOAK_SERVER_URL"))
+        return Response({"message": "Hello, World!"}, status=status.HTTP_200_OK)
