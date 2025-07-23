f5477a9e91f355294d718083743ded47c2d8b6b9
Esmat-Farjad <esmatullah.hashimi2016@gmail.com>
2025-07-23 23:04:40 +0430

    base model created

--- src/base/models.py
+++ src/base/models.py
@@ -12,3 +12,25 @@ class BaseUUIModel(models.Model):
 
     class Meta:
         abstract = True
+
+
+class Country(models.Model):
+    code = models.CharField(max_length=10)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.code} {self.name}"
+
+
+class Location(models.Model):
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+    zip = models.IntegerField()
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    city = models.CharField(max_length=100)
+    state_id = models.CharField(max_length=100)
+    state_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.city} {self.state_id}"
